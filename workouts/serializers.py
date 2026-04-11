45763b01c9ad2f4e2c6032ebd384b44cec0679c4
Oliviazkr <sc22kz@leeds.ac.uk>
2026-04-11 16:41:41 +0800

    Delete workouts directory

--- workouts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Workout
-
-# 把数据库数据转JSON，DRF自动处理，返回符合要求的JSON响应
-class WorkoutSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Workout
-        fields = '__all__'  # 所有字段都转JSON，包括自动生成的ID
